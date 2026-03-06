259858e2306e1a745f4e530b46469a4044acfbfd
Abhishek <abhishekkanichery@gmail.com>
2026-03-06 16:37:40 +0530

    added loop to stay on the program

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,22 +4,27 @@ use std::io;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input you guess.");
 
     let secret_number: u32 = rand::random_range(0..=100);
     println!("The secret number is: {secret_number}");
 
-    let mut guess: String = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read input!");
-    let guess_number: u32 = guess.trim().parse().expect("Failed to parse user input!");
+    loop {
+        println!("Please input your guess.");
 
-    match guess_number.cmp(&secret_number) {
-        cmp::Ordering::Less => println!("Too small!"),
-        cmp::Ordering::Greater => println!("Too big!"),
-        cmp::Ordering::Equal => println!("You win!"),
+        let mut guess: String = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read input!");
+        let guess_number: u32 = guess.trim().parse().expect("Failed to parse user input!");
+        println!("You guessed: {guess}");
+
+        match guess_number.cmp(&secret_number) {
+            cmp::Ordering::Less => println!("Too small!"),
+            cmp::Ordering::Greater => println!("Too big!"),
+            cmp::Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
-
-    println!("You guessed: {guess}")
 }
