f12bdd125b324eacfe50827095178486331925f3
Abhishek <abhishekkanichery@gmail.com>
2026-03-06 16:10:59 +0530

    random number generated

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,8 +1,12 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
     println!("Please input you guess.");
+
+    let secret_number = rand::random_range(0..=100);
+    println!("The secret number is: {secret_number}");
 
     let mut guess = String::new();
 
